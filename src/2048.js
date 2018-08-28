02d69c17b6f7a4cfdbc50fb387a480245bde484c
LuoHuan <472029437@qq.com>
2018-08-28 09:18:14 +0800

    study 2048 laya

---
+++ src/2048.js
@@ -0,0 +1,12 @@
+var WebGL = laya.webgl.WebGL;
+var Text = Laya.Text;
+Laya.init(375, 667, WebGL);
+Laya.stage.bgColor = "#faf8ee";
+
+var Main = function(){
+    let title = new Text();
+    title.font = '40px';
+    title.font-weight = '40px';
+    title.font = '40px';
+    title.font = '40px';
+}();
